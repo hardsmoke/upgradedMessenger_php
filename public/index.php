9acf7ce9e24ffecf0edab6427c30309b9e2084b0
Aleksandr Martynov <77721717+hardsmoke@users.noreply.github.com>
2022-06-18 15:01:12 +1000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,5 @@ use Messenger\Application;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$requestedPath = explode('?', $_SERVER['REQUEST_URI'])[0];
-
 $application = new Application();
 echo $application->run();
