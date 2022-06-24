9264b832374a79efc2fb46673869eaaa4e94fb24
Aleksandr Martynov <77721717+hardsmoke@users.noreply.github.com>
2022-06-24 13:24:25 +1000

    Update UserMapper.php

--- src/Model/Mapper/UserMapper.php
+++ src/Model/Mapper/UserMapper.php
@@ -16,24 +16,55 @@ class UserMapper implements \JsonSerializable
 
 	public function Add(User $user)
 	{
-		$username = $user->GetUsername();
-        $password = $user->GetPassword();
+	    $username = $user->GetUsername();
+            $password = $user->GetPassword();
 
-        $query = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
-        $this->connection->query($query);
+            $query = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
+            $this->connection->query($query);
 	}
 	
 	public function FindByName(string $username) : User
 	{
-		$query = "SELECT * FROM users WHERE users.username = '$username'";
-        $usersParams = $this->connection->query($query)->fetch(PDO::FETCH_ASSOC);
-        $user = new User('', '');
+	    $query = "SELECT * FROM users WHERE users.username = '$username'";
+            $usersParams = $this->connection->query($query)->fetch(PDO::FETCH_ASSOC);
+            $user = new User('', '');
 
         if ($usersParams['username'] != null)
         {
             return new User($usersParams['username'], $usersParams['password']);
         }
 
-        return $user;
+            return $user;
 	}
+	
+	public function Delete(int $id) : bool
+    	{
+	    $query = $this->connection->prepare('DELETE FROM users WHERE id = :id');
+            return $query->execute();
+    	}
+
+        public function GetAll() : array
+        {
+            $query = $this->connection->prepare('SELECT username FROM users');
+	
+	    $query->execute();
+	    $data = $query->fetchAll();
+            $users = array();
+            foreach ($data as $array)
+	    {
+                $users[] = new User($array['username'], $array['password']);
+            }
+	    
+        return $users;
+        }
+	
+    public function GetById(int $id) : User
+    {
+	$query = $this->pdo->prepare('SELECT * FROM users WHERE id = :id');
+	    
+        $query->execute(['id' => $id]);
+        $data = $query->fetch();
+	
+        return new User($data['username'], $data['password']);
+    }
 }
